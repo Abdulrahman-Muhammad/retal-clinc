77831a67cc6186c6b16f8ea11cba2752db214cc2
Abdelrahman <abdelrahman.elnegery@lexmodo.com>
2022-07-14 04:57:06 +0200

    auth

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\IndexController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\ShowController;
-
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,6 +25,24 @@ Route::get('/home', function () {
     return view('home');
 });
 
+Auth::routes();
+
+Route::get('/login' , function () {
+    return view('auth.login');
+});
+
+Route::get('/register', function () {
+    return view('auth.register');
+});
+
 Route::get('/addPatient', [IndexController::class, 'HandleRequest']);
 
 Route::get('/showDataOfPatients', [ShowController::class, 'ShowDataOfPatients']);
+
+// Route::post('register', [RegisterController::class, 'register'])->name('register');
+
+// Route::post('login', [LoginController::class, 'login'])->name('login');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
